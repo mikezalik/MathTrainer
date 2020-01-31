fc2bbc78eccd9533448495e5f94e6d8c95a6880f
mikezalik <mike.zalik@gmail.com>
2020-01-31 14:43:16 -0800

    Adds start button and method to render the start button invisible on click.

--- app/src/main/java/com/mikezalik/mathtrainer/MainActivity.java
+++ app/src/main/java/com/mikezalik/mathtrainer/MainActivity.java
@@ -3,12 +3,22 @@ package com.mikezalik.mathtrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
     }
 }
