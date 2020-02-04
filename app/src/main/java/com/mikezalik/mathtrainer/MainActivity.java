df8d8568bc4cd8fef4b62449519db9e310d5141e
mikezalik <mike.zalik@gmail.com>
2020-02-03 18:01:41 -0800

    Refactors import statements to remove unused code.

--- app/src/main/java/com/mikezalik/mathtrainer/MainActivity.java
+++ app/src/main/java/com/mikezalik/mathtrainer/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
